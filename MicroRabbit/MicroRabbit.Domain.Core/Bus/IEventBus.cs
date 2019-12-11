3b6ad6f27c1fd0b165bd5bbb48f2b5cc51af795d
winseng.ws <ba906660@hotamil.com>
2019-12-11 13:55:19 +0800

    Implemented interfaces and classes for Domain.Core

---
+++ MicroRabbit/MicroRabbit.Domain.Core/Bus/IEventBus.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using MicroRabbit.Domain.Core.Commands;
+using MicroRabbit.Domain.Core.Events;
+
+namespace MicroRabbit.Domain.Core.Bus
+{
+    public interface IEventBus
+    {
+        Task SendCommand<T>(T command) where T : Command;
+        void Publish<T>(T @event) where T : Event;
+        void Subscribe<T, TH>(T @event)
+            where T : Event
+            where TH : IEventHandler<T>;
+
+    }
+}
---
+++ MicroRabbit/MicroRabbit.Domain.Core/Events/Message.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MediatR;
+
+namespace MicroRabbit.Domain.Core.Events
+{
+    public abstract class Message : IRequest<bool>
+    {
+        public string MessageType { get; protected set; }
+        protected Message()
+        {
+            MessageType = GetType().Name;
+        }
+    }
+}
