e10f1225c851eed5117a73c8bbac999ad3f6ade5
winseng.ws <ba906660@hotamil.com>
2019-12-25 16:31:46 +0800

    Partially completed Banking Microservice

---
+++ MicroRabbit/MicroRabbit.Banking.Api/Controllers/BankingController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Application.Services;
+using MicroRabbit.Banking.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MicroRabbit.Banking.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BankingController : ControllerBase
+    {
+        private readonly IAccountService _accountService;
+
+        public BankingController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        // GET api/banking
+        [HttpGet]
+        public ActionResult<IEnumerable<Account>> Get()
+        {
+            return Ok(_accountService.GetAccounts());
+        }
+
+        //// GET api/values/5
+        //[HttpGet("{id}")]
+        //public ActionResult<string> Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        //// POST api/values
+        //[HttpPost]
+        //public void Post([FromBody] string value)
+        //{
+        //}
+
+        //// PUT api/values/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        //// DELETE api/values/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
---
+++ MicroRabbit/MicroRabbit.Banking.Data/Context/BankingDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MicroRabbit.Banking.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MicroRabbit.Banking.Data.Context
+{
+    public class BankingDbContext : DbContext
+    {
+        public BankingDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Account> Accounts { get; set; }
+    }
+}
