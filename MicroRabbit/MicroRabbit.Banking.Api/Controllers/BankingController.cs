5730b62cbc7177e4d101f7b599bd63cb6b4c0510
winseng.ws <ba906660@hotamil.com>
2019-12-26 14:32:00 +0800

    Implemented Publish and SendCommand for Banking Microservice

--- MicroRabbit/MicroRabbit.Banking.Api/Controllers/BankingController.cs
+++ MicroRabbit/MicroRabbit.Banking.Api/Controllers/BankingController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Application.Models;
 using MicroRabbit.Banking.Application.Services;
 using MicroRabbit.Banking.Domain.Models;
 using Microsoft.AspNetCore.Mvc;
@@ -25,6 +26,13 @@ namespace MicroRabbit.Banking.Api.Controllers
         public ActionResult<IEnumerable<Account>> Get()
         {
             return Ok(_accountService.GetAccounts());
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] AccountTransfer accountTransfer)
+        {
+            _accountService.Transfer(accountTransfer);
+            return Ok(accountTransfer);
         }
 
         //// GET api/values/5
--- MicroRabbit/MicroRabbit.Banking.Application/Services/AccountService.cs
+++ MicroRabbit/MicroRabbit.Banking.Application/Services/AccountService.cs
@@ -2,23 +2,42 @@
 using System.Collections.Generic;
 using System.Text;
 using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Application.Models;
+using MicroRabbit.Banking.Domain.Commands;
 using MicroRabbit.Banking.Domain.Interfaces;
 using MicroRabbit.Banking.Domain.Models;
+using MicroRabbit.Domain.Core.Bus;
 
 namespace MicroRabbit.Banking.Application.Services
 {
     public class AccountService : IAccountService
     {
         private readonly IAccountRepository _accountRepository;
+        private readonly IEventBus _bus;
 
-        public AccountService(IAccountRepository accountRepository)
+        public AccountService(IAccountRepository accountRepository, IEventBus bus)
         {
             _accountRepository = accountRepository;
+            _bus = bus;
         }
 
         public IEnumerable<Account> GetAccounts()
         {
             return _accountRepository.GetAccounts();
+        }
+
+
+        // where we start to make use our event bus to use mediator to send a command over and then
+        // that will trigger in our handler the publish event to the rabbitmq.
+        public void Transfer(AccountTransfer accountTransfer)
+        {
+            var createTransferCommand = new CreateTransferCommand(
+                accountTransfer.FromAccount,
+                accountTransfer.ToAccount,
+                accountTransfer.TransferAmount
+                );
+
+            _bus.SendCommand(createTransferCommand);
         }
     }
 }
